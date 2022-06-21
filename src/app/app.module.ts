bfe569c3b76a64599d678ad0a6ae6ab9ff177bf4
Benjamin Araoz <admin@dataindev.com>
2022-06-21 12:47:16 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormsComponent } from './Components/forms/forms.component';
 import { ListComponent } from './Components/list/list.component';
 import { PersonasService } from './Services/personas.service';
-import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
